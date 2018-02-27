428de5bad23ffcb660d9c20528d8ccf40a88e03b
Galen Han <Mastergalen@users.noreply.github.com>
2018-02-27 00:01:20 +0000

    Fix flow_predict
    (vx,vy)

---
+++ README.md
@@ -0,0 +1,6 @@
+* Run `heliotrope`
+* Enter your desired starting image
+* Draw as many points as you like to form a path
+* Double-click your final point to end selection
+
+TIP: Click the tip of the nose initially.
--- src/flow_predict.m
+++ src/flow_predict.m
@@ -2,6 +2,7 @@ function [ destination ] = flow_predict( path, flows_file, start )
 %FLOW_PREDICT Summary of this function goes here
 %   Detailed explanation goes here
 
+debug = false;
 downscale_factor = 0.3;
 
 destination = round(start * downscale_factor);
@@ -15,13 +16,27 @@ for i = 2:length(path)
         k = (b-1) * (b-2) / 2 + a;
     end
     
-    dv = squeeze(flows_file.flows_a(round(destination(1)), round(destination(2)), :, k))';
+    flow = flows_file.flows_a(:, :, :, k);
+    
+    if debug
+        figure
+        hold on;
+        quiver(flow(:, :, 1), flow(:, :, 2))
+        % opflow = opticalFlow(flow(:, :, 1), flow(:, :, 2));
+        % plot(opflow, 'DecimationFactor', [10, 10])
+        scatter(0, 100, 50, 'r.')
+    end
+    
+    dv = squeeze(flow(round(destination(1)), round(destination(2)), :))';
     
     if a < b
         dv = -dv;
     end
     
-    destination = destination + dv;
+    % dv = (vx,vy)
+    % destination = (y,x)
+    % Need to reverse order of dv 
+    destination = destination + [dv(2), dv(1)];
 end
 
 destination = round(destination / downscale_factor);
